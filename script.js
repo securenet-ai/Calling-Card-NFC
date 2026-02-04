8c8b1ed2d89076e3a693f9c6e15126fcc7061eff
securenet-ai <securenet579@gmail.com>
2026-02-04 13:49:59 +0800

    df
    dfddfsf

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const cardImages = {
     'panjsupapo': 'img/panj-img.png',
     'AttyJohnDerekPorciuncula': 'img/atty3-img.png',
     'john-doe': 'img/atty-img.png',
+    'ArchieEstrada': 'img/sirArchie-img.png',
     'default': 'img/atty1-img.png'
     
 };
